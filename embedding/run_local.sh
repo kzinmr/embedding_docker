d62f2feb25853b3bbeec84d8c6d0cc82c6e6165d
Kazuki Inamura <kzinmr109@gmail.com>
2021-01-21 20:14:39 +0900

    fix

--- embedding/run_local.sh
+++ embedding/run_local.sh
@@ -4,11 +4,10 @@ min_count=10
 dimension=256
 max_vocab_size=50000
 max_sentence_length=30000  # num tokens
-python3 preprocess.py
+# python3 preprocess.py
 
 w2v_model=/app/workspace/models/word2vec_sg_dim${dimension}_w${window}_mc${min_count}.model
 python3 train_word2vec.py \
--i $wikipedia_wakati \
 -o $w2v_model \
 --min-count $min_count \
 --max-vocab-size $max_vocab_size \
